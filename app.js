45cffe9dee4b59c0b8d3a33cbb0ee6a8e6710bc1
Ziv Kaplan <zivkaplan@gmail.com>
2021-05-25 12:44:41 +0300

    section 58 finished

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const passport = require('passport');
 const localStrategy = require('passport-local');
 const expressError = require(__dirname + '/utilities/expressError');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 const port = 3000;
 const app = express();
 
@@ -21,15 +23,8 @@ const reviewRouter = require('./routes/reviews');
 const userRouter = require('./routes/users');
 
 const appConfig = (function () {
-    app.engine('ejs', ejsMate);
-    app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, 'views'));
-
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.urlencoded({ extended: true }));
-    app.use(methodOverride('_method'));
-
     const sessionConfig = {
+        name: 'SessConnect',
         secret: 'defaultSecret',
         resave: false,
         saveUninitialized: true,
@@ -37,10 +32,69 @@ const appConfig = (function () {
             expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
+            // secure: true,
         },
     };
+    app.engine('ejs', ejsMate);
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(methodOverride('_method'));
     app.use(session(sessionConfig));
     app.use(flash());
+    app.use(mongoSanitize());
+
+    //helmet
+    app.use(helmet());
+
+    const scriptSrcUrls = [
+        'https://stackpath.bootstrapcdn.com/',
+        'https://api.tiles.mapbox.com/',
+        'https://api.mapbox.com/',
+        'https://kit.fontawesome.com/',
+        'https://cdnjs.cloudflare.com/',
+        'https://cdn.jsdelivr.net',
+    ];
+    const styleSrcUrls = [
+        'https://kit-free.fontawesome.com/',
+        'https://stackpath.bootstrapcdn.com/',
+        'https://api.mapbox.com/',
+        'https://api.tiles.mapbox.com/',
+        'https://fonts.googleapis.com/',
+        'https://use.fontawesome.com/',
+        'https://cdn.jsdelivr.net',
+    ];
+    const connectSrcUrls = [
+        'https://api.mapbox.com/',
+        'https://a.tiles.mapbox.com/',
+        'https://b.tiles.mapbox.com/',
+        'https://events.mapbox.com/',
+    ];
+    const fontSrcUrls = [];
+
+    app.use(
+        helmet.contentSecurityPolicy({
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", 'blob:'],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    'blob:',
+                    'data:',
+                    'https://images.unsplash.com/',
+                    'https://source.unsplash.com/collection/',
+                    'https://res.cloudinary.com/djrifev4q/',
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+            },
+        })
+    );
 
     //passport
     app.use(passport.initialize());
@@ -73,7 +127,6 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.path = req.originalUrl || '/';
-
     next();
 });
 
--- models/validationSchemas.js
+++ models/validationSchemas.js
@@ -1,25 +1,49 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!',
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value)
+                    return helpers.error('string.escapeHTML', { value });
+                return clean;
+            },
+        },
+    },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
-        location: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
+        location: Joi.string().required().escapeHTML(),
         price: Joi.number().min(0).required(),
-        description: Joi.string(),
+        description: Joi.string().escapeHTML(),
     }).required(),
     deleteImages: Joi.array(),
 });
 
 module.exports.imagesSchema = Joi.array().items(
     Joi.object({
-        path: Joi.string().required(),
-        filename: Joi.string().required(),
+        path: Joi.string().required().escapeHTML(),
+        filename: Joi.string().required().escapeHTML(),
     })
 );
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().min(1).max(5).required(),
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
     }).required(),
 });
